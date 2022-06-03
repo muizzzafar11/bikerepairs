a42c06cee8dfbcfa1aa006d3d75b167c4d48787a
Muizz Zafar <muizzzafar25351964@gmail.com>
2022-06-03 03:52:35 -0400

    Added routing options

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mb-4">
+    <BNav class="justify-content-between align-items-center">
+      <div>
+        <router-link to="/" class="text-white">
+          <img src="@/assets/home_logo.png" style="width: 150px">
+        </router-link>
+      </div>
+      <div class="d-flex justify-content-end">
+        <BNav-item>
+          <router-link to="/" class="text-white">Home</router-link>
+        </BNav-item>
+        <BNav-item>
+          <router-link to="/booking" class="text-white">Bookings</router-link>
+        </BNav-item>
+        <BNav-item>
+          <router-link to="/team" class="text-white">Team</router-link>
+        </BNav-item>
+        <BNav-item>
+          <router-link to="/about" class="text-white">About</router-link>
+        </BNav-item>
+      </div>
+    </BNav>
+  </div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Comfortaa');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Comfortaa';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* text-align: center; */
+  /* color: #2c3e50; */
+  padding: 20px;
+  background: linear-gradient(269.69deg, #141414 0.22%, #373737 95.97%);
+  min-height: 100vh;
+  /* margin-top: 60px; */
+}
+p, h1, h2, h3, h4 {
+  color: white;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,17 +4,21 @@ import * as VueRouter from 'vue-router'
 import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import vSelect from 'vue-select'
+import 'vue-select/dist/vue-select.css';
 
 import HomeScreen from './Screens/LandingScreen.vue'
 import RepairScreen from './Screens/RepairScreen.vue'
 import AboutScreen from './Screens/AboutScreen.vue'
 import TeamScreen from './Screens/TeamScreen.vue'
+import ReceiptScreen from './Screens/ReceiptScreen.vue'
 
 const routes = [
     { path: '/', name: "Home", component: HomeScreen },
-    { path: '/repair', name: "Repair", component: RepairScreen },
+    { path: '/booking', name: "Repair Booking", component: RepairScreen },
     { path: '/about', name: "About", component: AboutScreen },
     { path: '/team', name: "Team", component: TeamScreen },
+    { path: '/receipt', name: "Booking Confirmation", component: ReceiptScreen },
 ]
 
 const router = VueRouter.createRouter({
@@ -25,4 +29,5 @@ const router = VueRouter.createRouter({
 createApp(App)
 .use(router)
 .use(BootstrapVue3)
+.component('v-select', vSelect)
 .mount('#app')
