48c9826c3852e169a3b489358a2c969a77ed5c1a
Muizz Zafar <muizzzafar25351964@gmail.com>
2022-06-03 03:54:46 -0400

    added scheduling screen

--- src/Screens/SchedulingScreen.vue
+++ src/Screens/SchedulingScreen.vue
@@ -1,58 +1,82 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="simple-example px-5 text-center pb-5">
+    <h3 class="mb-4">Select Repair Time</h3>
+    <vue-meeting-selector
+      style="background: white; border-radius: 30px;"
+      ref="meetingSelector"
+      class="meeting-selector px-4 py-4"
+      v-model="meeting"
+      :loading="false"
+      :meetings-days="meetingsDays"
+    />
+  </div>
+  <div class="px-5 py-5 text-center">
+      <h3 class="mb-4">Select Repair Time</h3>
+      <b-form-select class="py-2" v-model="selected" :options="options" style="border-radius: 10rem"></b-form-select>
+  </div>
+  <div class="px-5 py-5  text-center">
+      <h3 class="mb-4">Select Repair Time</h3>
+      <b-form-input class="py-2" v-model="name" placeholder="Enter your name" style="border-radius: 10rem"></b-form-input>
+      <b-form-input class="mt-3 py-2" v-model="email" placeholder="Enter your email" style="border-radius: 10rem"></b-form-input>
+      <b-form-input class="mt-3 py-2" v-model="phone" placeholder="Enter your phone" style="border-radius: 10rem"></b-form-input>
+  </div>
+  <div class="text-center">
+      <BButton pill class="px-5 py-2 mt-2" variant="light" @click="goToNextStep()">Confirm Booking</BButton>
   </div>
 </template>
 
 <script>
+import VueMeetingSelector from "vue-meeting-selector";
+import slotsGenerator from "vue-meeting-selector/src/helpers/slotsGenerator";
 export default {
-  name: 'SchedulingScreen',
-  props: {
-    msg: String
-  }
+  name: 'AboutScreen',
+  components: {
+    VueMeetingSelector,
+  }, data() {
+    return {
+      selected: null,
+      options: ['list', 'of', 'options'],
+      meetingSelector: null,
+      meeting: null,
+      name: null,
+      email: null,
+      phone: null,
+      meetingsDays: slotsGenerator(
+        new Date(),
+        7,
+        {
+          hours: 8,
+          minutes: 0,
+        }, 
+        {
+          hours: 16,
+          minutes: 0,
+        }, 
+        30
+      ),
+    }
+  }, methods: {
+    goToNextStep() {
+      // var date = new Date(this.meeting.date);
+      // console.log(date);
+      this.$router.push('/receipt');
+      // this.$router.push('/booking');
+    },
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.meeting__button {
+  background-color: black !important;
+  color: white !important;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.meeting__button--selected {
+  background-color: grey !important;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.meeting-selector .tab__pagination__button {
+  color: grey !important;
 }
 </style>
