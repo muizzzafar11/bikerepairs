039352a643856ae862ccb11aee49793e36e575c6
Muizz Zafar <muizzzafar25351964@gmail.com>
2022-06-03 03:53:10 -0400

    updated home screen

--- src/Screens/LandingScreen.vue
+++ src/Screens/LandingScreen.vue
@@ -1,10 +1,26 @@
 <template>
-<div>Home Screen</div>
+  <div style="height: 100%; overflow: scroll;" class="d-flex">
+    <div class="align-self-center">
+      <h1 class="display-3">Bikes perfectly attuned to your needs</h1>
+      <h3 class="mt-5">Our shop provides a variety of products and bicycle parts for you to enjoy your bike ride to the fullest</h3>
+    </div>
+    <div class="d-flex">
+      <img class="img-fluid" style="transform: scaleX(-1);" src="@/assets/bike.png">
+    </div>
+  </div>
+  <div class="text-center mt-5">
+    <BButton pill class="px-5 py-2" variant="light" @click="goToRepair()">Schedule a repair</BButton>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'HomeScreen',
+  methods: {
+    goToRepair() {
+      this.$router.push('/booking');
+    }
+  }
 }
 </script>
 
